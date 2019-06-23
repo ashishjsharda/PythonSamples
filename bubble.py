c3c46d5381278a85066708cb6df1a126ea0ce629
Ashish S <ashishjsharda@gmail.com>
2019-06-22 20:03:10 -0400

    bubble.py
    bubble.py

---
+++ bubble.py
@@ -0,0 +1,17 @@
+'''
+Created on Jun 22, 2019
+
+@author: asharda
+'''
+
+import matplotlib.pyplot as plt
+import numpy as np
+ 
+# create data
+x = np.random.rand(40)
+y = np.random.rand(40)
+z = np.random.rand(40)
+colors = np.random.rand(40) 
+# use the scatter function
+plt.scatter(x, y, s=z*1000,c=colors)
+plt.show()
