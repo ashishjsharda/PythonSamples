eeeeaf700b559e6eb7153bf918b2cd199b85b1e0
Ashish S <ashishjsharda@gmail.com>
2019-07-16 06:05:10 -0400

    Sets in Python
    Sets in Python

---
+++ sets.py
@@ -0,0 +1,14 @@
+'''
+Created on Jul 16, 2019
+
+@author: asharda
+'''
+#Create a Set
+animals={"cat","dogs"}
+#Check if an element is in a set
+print("cat" in animals)
+#Print Length
+print(len(animals))
+#Add a new element
+animals.add("Tiger")
+print(len(animals))
