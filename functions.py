7f050a9faf5d27a59a78624b0717f7b45e4c840d
Ashish S <ashishjsharda@gmail.com>
2019-08-01 18:34:49 -0400

    Using functions in python
    Using functions in python

---
+++ functions.py
@@ -0,0 +1,12 @@
+'''
+Created on August 1, 2019
+Functions in Python
+@author: asharda
+'''
+
+
+def printList(mylist):
+    for n in mylist:
+        print(n)
+mylist=[1,2,3,4,5]
+printList(mylist)
