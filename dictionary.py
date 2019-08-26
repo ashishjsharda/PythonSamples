cf46b322f1598aaee71fe48dc4ef3b32356e7cc3
Ashish S <ashishjsharda@gmail.com>
2019-08-26 04:47:49 -0400

    dictionary.py
    dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+'''
+Created on Aug 26, 2019
+Dictionary in Python
+@author: asharda
+'''
+dict={'Name':'Sai','Age':10000}
+print(dict['Name'])
+print(dict['Age'])
+print(dict)
+dict.clear()
+print(dict)
