ee0db54cbd474c5b54dc68a2556aef830d0900fa
Ashish S <ashishjsharda@gmail.com>
2019-08-27 06:58:59 -0400

    Calendar and Time in Python
    Calendar and Time in Python

---
+++ calendartime.py
@@ -0,0 +1,9 @@
+'''
+Created on Aug 26, 2019
+Calendar and Time 
+@author: asharda
+'''
+import calendar
+import time
+print(calendar.month(2019,10))
+print(time.asctime(time.localtime(time.time())))
