06abb8a0dbd31708ce00a7330d4e249631178ffc
Ashish S <ashishjsharda@gmail.com>
2019-09-01 16:42:04 -0400

    Matplotlib Example
    Matplotlib Example

---
+++ matplotlib3.py
@@ -0,0 +1,10 @@
+'''
+Created on Aug 31, 2019
+Using Matplotlib 
+@author: asharda
+'''
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4,5])
+plt.ylabel("Y-Axis")
+plt.xlabel("X-Axis")
+plt.show()
