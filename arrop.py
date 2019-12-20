d4ba7521380faf49a245e669e84d295987c9a540
Ashish S <ashishjsharda@gmail.com>
2019-12-20 07:54:40 -0500

    Create arrop.py

---
+++ arrop.py
@@ -0,0 +1,11 @@
+'''
+Created on Dec 20, 2019
+Array Operations using numpy
+@author: asharda
+'''
+import numpy as np
+a=np.array([1,2,3,4,5])
+a[0]=6
+print("Type of a is",type(a))
+print("Size of array is ",np.shape(a))
+print(a)
