104efe5c4a62f7207eaada643f1de2534c9a3605
Ashish S <ashishjsharda@gmail.com>
2019-12-23 21:14:58 -0500

    Create numpy10.py

---
+++ numpy10.py
@@ -0,0 +1,12 @@
+'''
+Created on Dec 23, 2019
+
+@author: ashish
+'''
+import numpy as np
+#Create a constant array
+a=np.full((2,2),7)
+print(a)
+#Generate a random array
+rand_arr=np.random.random((2,2))
+print(rand_arr)
