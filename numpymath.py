1d85b40cfd0c0bd3042faca8de0efdb8b187c65f
Ashish S <ashishjsharda@gmail.com>
2019-12-25 14:49:33 -0500

    Create numpymath.py

---
+++ numpymath.py
@@ -0,0 +1,14 @@
+'''
+Created on Dec 25, 2019
+
+@author: ashish
+'''
+import numpy as np
+a=np.array([1,2])
+b=np.array([1,2])
+add=np.add(a,b)
+print(add)
+sub=np.subtract(a,b)
+print(sub)
+mul=np.multiply(a,b)
+print(mul)
