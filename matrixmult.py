ce8c83da2e52e3dc5df734a43ae14b047fd0dbdc
Ashish S <ashishjsharda@gmail.com>
2019-12-26 08:08:42 -0500

    Create matrixmult.py

---
+++ matrixmult.py
@@ -0,0 +1,12 @@
+'''
+Created on Dec 26, 2019
+Using Dot Operation
+@author: ashish
+'''
+import numpy as np
+a=np.array([[1,2],[3,4]])
+b=np.array([[5,6],[7,8]])
+#Matrix Multiplication
+print(np.dot(a,b))
+
+
