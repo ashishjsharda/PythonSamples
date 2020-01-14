60e74ccebfc72cea21877c1ae3fd5229abde270a
Ashish S <ashishjsharda@gmail.com>
2020-01-13 21:39:37 -0500

    Create stringop.py

---
+++ stringop.py
@@ -0,0 +1,11 @@
+'''
+Created on Jan 12, 2020
+
+@author: ashish
+'''
+course="Python for Begineers"
+print(course.replace('P', 'N'))
+print(course.lower())
+print(course.upper())
+print("Boolean value",course.isupper())
+print(course.title())
